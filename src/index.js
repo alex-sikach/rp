a416933a51fd3fdfd7be37b158ebd576d2ec3bc3
alex-sikach <hlophlop1245@gmail.com>
2023-01-08 19:51:03 +0200

    User friendly message about faking cookies

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import App from './App'
 import './index.css'
 
+console.log("%cIf you accidentally changed your cookie - session just delete this cookie at all, delete local storage - authed and refresh the page.", "color: blue; font-size: 20px")
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
